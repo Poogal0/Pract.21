12f6c48aa800d5b99172f66adcc7d4d3ce1ab60d
Poogal0 <golubchikov_2002@mail.ru>
2023-06-01 10:07:27 +0400

    v4 - .js file added

--- maps.js
+++ maps.js
@@ -1,6 +1,23 @@
 console.log("Test yandex panorama v0.1: OK.")
 ymaps.ready(init);
-
+var x0 = 55.790927;
+    y0 = 49.114453;
 function init() {
-	console.log("ymaps: OK");	
+	if (!ymaps.panorama.isSupported) {
+		console.log("API maps error.");
+	}
+	console.log("ymaps: OK");
+	ymaps.panorama.locate([x0,y0]).done(
+	isPanoramaFound,
+	errorHandled()
+	);
+}
+
+function isPanoramaFound() {
+	console.log("isPanoramasFound: Callid.");
+	
+}
+function errorHandled() {
+	console.log("errorHandled: Callid.");
+	
 }
